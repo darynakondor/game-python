9ba72e075851ce609e0a393422702216bce1782c
Daryna <condordaryna@gmail.com>
2022-11-18 20:53:28 +0200

    add control of the ball with the keyboard

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
 screen = width, heigth = 800, 600
 
-LIST_OF_BALL_COLORS = [(255, 255, 255), (255, 0, 0),
-                       (60, 179, 113), (238, 130, 238), (0, 0, 255)]
+BALL_COLOR = 255, 255, 255
 BACKGROUND_COLOR = 0, 0, 0
 
 main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
-ball.fill((LIST_OF_BALL_COLORS[0]))
+ball.fill(BALL_COLOR)
 ball_rect = ball.get_rect()
-ball_speed = [1, 1]
+ball_speed = 1
 
 is_working = True
 
@@ -23,23 +22,22 @@ while is_working:
         if event.type == QUIT:
             is_working == False
 
-    ball_rect = ball_rect.move(ball_speed)
+    presed_keys = pygame.key.get_pressed()
 
-    if ball_rect.bottom >= heigth or ball_rect.top <= 0:
-        ball_speed[1] = -ball_speed[1]
-    if ball_rect.right >= width or ball_rect.left <= 0:
-        ball_speed[0] = -ball_speed[0]
-    if ball_rect.bottom >= heigth:
-        ball.fill((LIST_OF_BALL_COLORS[1]))
-    elif ball_rect.top <= 0:
-        ball.fill((LIST_OF_BALL_COLORS[2]))
-    elif ball_rect.right >= width:
-        ball.fill((LIST_OF_BALL_COLORS[3]))
-    elif ball_rect.left <= 0:
-        ball.fill((LIST_OF_BALL_COLORS[4]))
-
-    main_surface.fill((BACKGROUND_COLOR))
+    main_surface.fill(BACKGROUND_COLOR)
 
     main_surface.blit(ball, ball_rect)
+
+    if presed_keys[K_DOWN]:
+        ball_rect = ball_rect.move(0, ball_speed)
+
+    if presed_keys[K_UP]:
+        ball_rect = ball_rect.move(0, -ball_speed)
+
+    if presed_keys[K_RIGHT]:
+        ball_rect = ball_rect.move(ball_speed, 0)
+
+    if presed_keys[K_LEFT]:
+        ball_rect = ball_rect.move(-ball_speed, 0)
 
     pygame.display.flip()
