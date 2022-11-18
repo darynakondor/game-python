7a2ad9969cbc77bd284404a25a944aa90ec1080a
Daryna <condordaryna@gmail.com>
2022-11-18 21:17:42 +0200

    add enemy to game

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = width, heigth = 800, 600
 
 BALL_COLOR = 255, 255, 255
+ENEMY_COLOR = 255, 0, 0
 BACKGROUND_COLOR = 0, 0, 0
 
 main_surface = pygame.display.set_mode(screen)
@@ -15,18 +16,38 @@ ball.fill(BALL_COLOR)
 ball_rect = ball.get_rect()
 ball_speed = 1
 
+
+def create_enemy():
+    enemy = pygame.Surface((20, 20))
+    enemy.fill(ENEMY_COLOR)
+    enemy_rect = pygame.Rect(width, 100, *enemy.get_size())
+    enemy_speed = 1
+    return [enemy, enemy_rect, enemy_speed]
+
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
+
 is_working = True
 
 while is_working:
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working == False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
 
     presed_keys = pygame.key.get_pressed()
 
     main_surface.fill(BACKGROUND_COLOR)
 
     main_surface.blit(ball, ball_rect)
+
+    for enemy in enemies:
+        main_surface.blit(enemy[0], enemy[1])
+        enemy[1] = enemy[1].move(-enemy[2], 0)
 
     if presed_keys[K_DOWN]:
         ball_rect = ball_rect.move(0, ball_speed)
